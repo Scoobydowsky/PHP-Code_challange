f027e1d2b882eaa18dbf2119c89abd5ee8154acf
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2022-05-29 15:07:12 +0200

    2. Creating variables

---
+++ codeacademy.com - php_code_challenges_for_beginners/2 Challange/index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <title>1st Challange from www.codeacademy.com</title>
+</head>
+<body>
+<?php
+$mary = "Mary";
+$lamb = "lamb";
+
+echo "{$mary} had a little {$lamb}";
+?>
+</body>
+</html>
