0f37ba9e22feb2f6812cd96d8dfd35bbbbf63175
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2022-05-29 15:04:07 +0200

    1. Create a hello world page

---
+++ codeacademy.com - php_code_challenges_for_beginners/1 Challange/index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+    <head>
+        <title>1st Challange from www.codeacademy.com</title>
+    </head>
+    <body>
+    <?php
+        $hello = "Hello Word";
+
+        echo "<h1>{$hello}</h1>";
+    ?>
+    </body>
+</html>
+
+
