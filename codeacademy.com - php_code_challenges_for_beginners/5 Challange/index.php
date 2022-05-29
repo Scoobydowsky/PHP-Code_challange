de0bec547e51deaa71c4c5085ff00b4abdaff322
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2022-05-29 15:43:35 +0200

    5. Create a multiplication table using nested for loops

---
+++ codeacademy.com - php_code_challenges_for_beginners/5 Challange/index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <title>5th Challange from www.codeacademy.com</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>Number</th>
+        <th>x2</th>
+    </tr>
+    <?php
+    for($i = 1; $i <=12 ; $i++){
+
+    }
+    echo "<td>{$i}</td>";
+    ?>
+</table>
+
+</body>
+</html>
