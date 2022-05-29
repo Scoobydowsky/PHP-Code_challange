20b3d28a6f9a49abb12ea19a90e09d9e35bc2670
Tomasz Woytkowiak <tomas.woytkowiak1505@gmail.com>
2022-05-29 16:01:17 +0200

    6. Responding to a PHP form

---
+++ codeacademy.com - php_code_challenges_for_beginners/6 Challange/hello.php
@@ -0,0 +1,5 @@
+<?php
+$name = $_POST['username'];
+
+
+echo "Hi,your name is {$name}";
---
+++ codeacademy.com - php_code_challenges_for_beginners/6 Challange/index.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <title>5th Challange from www.codeacademy.com</title>
+</head>
+<body>
+    <form action="hello.php" method="post">
+        <label for="username">Name:</label><input type="text" name="username" id="username" required><br>
+        <input type="submit">
+    </form>
+</body>
+</html>
